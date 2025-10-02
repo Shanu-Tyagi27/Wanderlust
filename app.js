aac06e24781d80ecf1a44391709118fb96a11589
Shanu Tyagi <135420091+ShanuTyagi-coder@users.noreply.github.com>
2025-10-02 20:29:10 +0530

    Add root route to redirect / to /listings

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV != "production") {
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path"); //FOR VIEWS
+const path = require("path"); // FOR VIEWS
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const MongoStore = require("connect-mongo");
@@ -15,15 +15,15 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listingRouter = require("./routes/listing.js"); // This will connect the listing.js router here
-const reviewRouter = require("./routes/review.js"); // This will connect the review.js router here
-const userRouter = require("./routes/user.js"); //This will connect the user.js router here
+const listingRouter = require("./routes/listing.js"); // listings router
+const reviewRouter = require("./routes/review.js"); // reviews router
+const userRouter = require("./routes/user.js"); // users router
 
 const dbUrl = process.env.ATLASDB_URL;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true })); // USED TO PARSE THE DATA FROM req.params
+app.use(express.urlencoded({ extended: true })); // parse req.body
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
@@ -33,12 +33,12 @@ const store = MongoStore.create({
   crypto: {
     secret: process.env.SECRET,
   },
-  touchAfter:24*3600,
+  touchAfter: 24 * 3600,
 });
 
-store.on("error",()=>{
-  console.log("ERROR in MONGO SESSION STORE",err);
-})
+store.on("error", (err) => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
 
 const sessionOptions = {
   store,
@@ -52,51 +52,49 @@ const sessionOptions = {
   },
 };
 
-
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use(passport.initialize()); // from here
+app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate())); // every user and request will go through this
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser()); //to here all lines are to use passport
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.successAdded = req.flash("success");
   res.locals.successDeleted = req.flash("deletion");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user; // We are saving this to access the user in navbar.ejs
+  res.locals.currUser = req.user;
   next();
 });
 
 async function main() {
-  // await mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
   await mongoose.connect(dbUrl);
 }
 main()
-  .then((res) => {
+  .then(() => {
     console.log("Connected to Database");
   })
   .catch((err) => {
-    console.log("Some error occured", err);
+    console.log("Some error occurred", err);
   });
 
+// ---------------- ROOT ROUTE ----------------
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
-//Whole listings routes are removed and only this line is required
+// ROUTES
 app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter); // this reviews is coming from up(const reviews = require....)
+app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
-//  /listings/:id/reviews actually this whole route resides inside app.js and doesn't go inside review.js so for that
-
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { err });
-  //   res.status(statusCode).send(message);
 });
+
 app.listen(8080, () => {
   console.log("Server listening to port 8080");
 });
-
